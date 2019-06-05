c642b89d0874c7bb66c9673dd84e6e0ebe6ba638
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-04 23:44:04 -0700

    Clean up comments.

--- valkyrie-engine/src/app/models/item/Lore.ts
+++ valkyrie-engine/src/app/models/item/Lore.ts
@@ -3,13 +3,13 @@ import { ItemState } from './ItemState';
 
 /**
  *
- * @prop {Guid}        id Item's unique identifier.
- * @prop {string}      name Item's display name.
- * @prop {string}      description Details about the effect of this lore (optional).
- * @prop {ItemState[]} states States automatically applied to items of this lore. Used to trigger
- *                            icon display effects (! for new item, lock symbol for locked item,
- *                            outlines or overlays for rare, etc) and to enforce state behavior
- *                            like binding, locked items, items that can be active or passive, etc.
+ * @prop [id] Item's unique identifier.
+ * @prop [name] Item's display name.
+ * @prop [description] Details about the effect of this lore (optional).
+ * @prop [states] States automatically applied to items of this lore. Used to trigger
+ *                icon display effects (! for new item, lock symbol for locked item,
+ *                outlines or overlays for rare, etc) and to enforce state behavior
+ *                like binding, locked items, items that can be active or passive, etc.
  *
  */
 export interface Lore {
