3991930d95ba2a9ddc4c0b3840641fbb0a99882b
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-04 23:43:32 -0700

    Convert to abstract class, implement get/set.

--- valkyrie-engine/src/app/models/item/Item.ts
+++ valkyrie-engine/src/app/models/item/Item.ts
@@ -4,26 +4,87 @@ import { ItemState } from './ItemState';
 
 /**
  *
- * @prop {Guid}        id Item's unique identifier.
- * @prop {string}      name Item's display name.
- * @prop {string}      description Details about the item.
- * @prop {Guid}        icon Display icon's unique identifier.
- * @prop {Lore[]}      lore Array of lore attributed to item.
- * @prop {ItemState[]} state Array of states currently applied to item.
+ * @prop [id] Item's unique identifier.
+ * @prop [name] Item's display name.
+ * @prop [description] Details about the item.
+ * @prop [icon] Display icon's unique identifier.
+ * @prop [lore] Array of lore attributed to item.
+ * @prop [state] Array of states currently applied to item.
  *
  */
-export class Item {
+export abstract class Item {
   id: Guid;
   name: string;
+  description: string;
   icon: Guid;
   lore: Lore[];
-  state: ItemState[]; // TODO: ItemState rules
+  state: ItemState[];
 
-  constructor(name: string, icon: Guid, description: string, lore = new Array<Lore>(), state = new Array<ItemState>()) {
+  constructor(
+    name: string,
+    description: string,
+    icon: Guid,
+    lore = new Array<Lore>(),
+    state = new Array<ItemState>()
+  ) {
     this.id = Guid.create();
     this.name = name;
+    this.description = description;
     this.icon = icon;
     this.lore = lore;
     this.state = state;
   }
+
+  getId(): Guid {
+    return this.id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  getDescription(): string {
+    return this.description;
+  }
+
+  setDescription(description: string): void {
+    this.description = description;
+  }
+
+  getIcon(): Guid {
+    return this.icon;
+  }
+
+  setIcon(iconId: Guid): void {
+    this.icon = iconId;
+  }
+
+  getLore(): Lore[] {
+    return this.lore;
+  }
+
+  setLore(lore: string | Guid): void {
+    if (typeof(lore) === 'string') {
+      lore = Guid.parse(lore);
+    }
+
+  }
+
+  getState(): ItemState[] {
+    return this.state;
+  }
+
+  setState(state: string | Guid): void { // string state.name or Guid state.id
+    if (typeof(state) === 'string') {
+      state = Guid.parse(state);
+    }
+
+  }
+
+  abstract create(props: object): Item;
+  abstract destroy(): void;
 }
