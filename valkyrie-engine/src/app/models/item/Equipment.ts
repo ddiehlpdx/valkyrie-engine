aa86cbe4f57f1fd09037b7b54ad30f30e283e45a
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-10 20:02:07 -0700

    Start Equipment class set up.

--- valkyrie-engine/src/app/models/item/Equipment.ts
+++ valkyrie-engine/src/app/models/item/Equipment.ts
@@ -1,6 +1,33 @@
+import { Guid } from 'node_modules/guid-typescript';
 import { Item } from './Item';
 
+/**
+ *
+ * @prop [slot] Equipment slot; head, chest, main hand, feet, etc.
+ * @prop [professions] Array of unique identifiers of professions that can use item.
+ * @prop [stats] Item stats as key/value; { str: 5, dex: 2, def: 25, atk: 27 }, etc.
+ *
+ */
 export abstract class Equipment extends Item {
+  slot: Guid;
+  professions: Guid[];
+  stats: object;
 
-  abstract create(props: object): Item;
+  /**
+   *
+   * Callback method executed when Item is equipped to a unit.
+   *
+   */
+  onEquip(): void {
+
+  }
+
+  /**
+   *
+   * Callback method executed when Item is removed from a unit.
+   *
+   */
+  onRemove(): void {
+
+  }
 }
