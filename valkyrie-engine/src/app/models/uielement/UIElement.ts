9b6b904c7f004e1a0cab36aa5ee232ee2c1250c5
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-10 20:03:42 -0700

    Fix comment formatting.

--- valkyrie-engine/src/app/models/uielement/UIElement.ts
+++ valkyrie-engine/src/app/models/uielement/UIElement.ts
@@ -2,7 +2,7 @@ import { Guid } from 'node_modules/guid-typescript';
 
 /**
  *
- * @prop id [Guid] UI Element's unique identifier.
+ * @prop [id] UI Element's unique identifier.
  *
  */
 export class UIElement {
