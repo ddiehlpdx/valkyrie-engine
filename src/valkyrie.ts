d377f39b921a5ba633d160b4729da7b977cdd50f
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-17 19:27:40 -0700

    Import three.js.

--- src/valkyrie.ts
+++ src/valkyrie.ts
@@ -1,5 +1,5 @@
 import { Ability } from './models/ability/Ability';
-import { Action } from './models/battlefield/Action';
+import { Action } from './models/action/Action';
 import { Battlefield } from './models/battlefield/Battlefield';
 import { Terrain } from './models/battlefield/Terrain';
 import { Tile } from './models/battlefield/Tile';
@@ -8,12 +8,16 @@ import { Consumable } from './models/item/Consumable';
 import { Equipment } from './models/item/Equipment';
 import { Item } from './models/item/Item';
 import { ItemState } from './models/item/ItemState';
-import { Lore } from './models/item/Lore';
 import { Icon } from './models/uielement/Icon';
 import { UIElement } from './models/uielement/UIElement';
 import { Profession } from './models/unit/Profession';
 import { Unit } from './models/unit/Unit';
 
-export class Valkyrie {
+import * as THREE from '../node_modules/three';
+import { PerspectiveCamera, Scene, BoxGeometry, MeshNormalMaterial, Mesh, WebGLRenderer } from '../node_modules/three';
 
+export class Valkyrie {
+  init(): void {
+
+  }
 }
