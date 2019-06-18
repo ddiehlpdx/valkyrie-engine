1dc7d232499055e82289c62192a8e65efdd6e702
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-17 19:27:04 -0700

    Start Tile class.

--- src/models/battlefield/Tile.ts
+++ src/models/battlefield/Tile.ts
@@ -1,5 +1,15 @@
 import { Guid } from 'guid-typescript';
 
+/**
+ *
+ * 
+ *
+ */
 export class Tile {
-
+  id: Guid;
+  terrain: Guid;
+  texture: Guid;
+  height: number;
+  position: object;
+  traversable: boolean;
 }
