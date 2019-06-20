2dd3278a70f8d5c5918c4d090b487ca3a17885bf
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-20 16:26:17 -0700

    Start Profression model.

--- src/models/unit/Profession.ts
+++ src/models/unit/Profession.ts
@@ -1,15 +1,31 @@
 import { Guid } from 'guid-typescript';
-import { Ability } from '../ability/Ability';
-import { Action } from '../action/Action';
 
+/**
+ * 
+ * @prop [id] Profession's unique identifier.
+ * @prop [name] Profession's display name.
+ * @prop [description] Description of class for unit selection screen.
+ * @prop [stats] Starting stats for profession.
+ * @prop [abilities] Abilities available to profession, by level, as key value pairs,
+ *                   where unlock level is key and ability ID is value.
+ *                   { 10: [ability.id], 17: [ability.id], 24: [ability.id] }
+ * @prop [actions] Actions that this profession can take on the battlefield.
+ * 
+ */
 export class Profession {
   id: Guid;
   name: string;
-  abilities: Ability[];
+  description: string;
   stats: object;
-  actions: Action[];
+  abilities: object;
+  actions: Guid[];
 
-  constructor() {
+  constructor(params: any) {
     this.id = Guid.create();
+    this.name = params.name;
+    this.description = params.description;
+    this.stats = params.stats;
+    this.abilities = params.abilities;
+    this.actions = params.actions;
   }
 }
