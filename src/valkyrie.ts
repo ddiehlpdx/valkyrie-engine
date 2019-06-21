a81fdd6701fd1a51a22d1c149b703303d6440009
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-06-21 14:33:00 -0700

    Begin main engine class setup.

--- src/valkyrie.ts
+++ src/valkyrie.ts
@@ -3,7 +3,7 @@ import { Action } from './models/action/Action';
 import { Battlefield } from './models/battlefield/Battlefield';
 import { Terrain } from './models/battlefield/Terrain';
 import { Tile } from './models/battlefield/Tile';
-import { Inventory } from './models/inventory/Inventory';
+import { Inventory } from './models/player/Inventory';
 import { Consumable } from './models/item/Consumable';
 import { Equipment } from './models/item/Equipment';
 import { Item } from './models/item/Item';
@@ -13,10 +13,29 @@ import { UIElement } from './models/uielement/UIElement';
 import { Profession } from './models/unit/Profession';
 import { Unit } from './models/unit/Unit';
 
-import * as THREE from '../node_modules/three';
+import * as THREE from 'three';
 
-export class Valkyrie {
+export default abstract class Valkyrie {
+  constructor(params: ValkyrieConfiguration) {
+    console.log(params);
+  }
+
   init(): void {
 
   }
+
+  test(): boolean {
+    if (confirm('Does this work?')) {
+      return true;
+    }
+    else return false;
+  }
+}
+
+export interface ValkyrieConfiguration {
+  windowWidth?: string | number,        // Width of game window; default window.innerWidth
+  windowHeight?: string | number,       // Height of game window; default window.innerHeight
+  dbName?: string,                      // Database name where game data is stored (if applicable); default ''
+  dbLogin?: string,                     // Login for database; default ''
+  dbPassword?: string,                  // Password for database; default ''
 }
