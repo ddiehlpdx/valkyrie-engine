01847b7362c5af2449b6df99e1d28c5e28e1c7b9
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-08-03 15:22:13 -0700

    Add configuration properties.

--- src/models/uielement/UIElement.ts
+++ src/models/uielement/UIElement.ts
@@ -1,5 +1,4 @@
 import { Guid } from 'guid-typescript';
-
 export { Icon } from './Icon';
 
 /**
@@ -9,8 +8,23 @@ export { Icon } from './Icon';
  */
 export abstract class UIElement {
   id: Guid;
+  name: string;
+  height: number;
+  width: number;
+  posX: number;
+  posY: number;
+  posZ: number | 'top' | 'bottom';
+  opacity: number;
+  
 
   constructor(params: any) {
     this.id = Guid.create();
+    this.name = params.name;
+    this.height = params.height;
+    this.width = params.width;
+    this.posX = params.posX || 0;
+    this.posY = params.posY || 0;
+    this.posZ = params.posZ || 'top';
+    this.opacity = params.opacity || 1;
   }
 }
