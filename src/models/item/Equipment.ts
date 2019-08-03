886dbb5a3403ba1f266c6b25a73752ae96ee5873
ddiehlpdx <ddiehl.pdx@gmail.com>
2019-08-03 15:21:59 -0700

    Combine weapon/armor into equipment class.

--- src/models/item/Equipment.ts
+++ src/models/item/Equipment.ts
@@ -8,23 +8,56 @@ import { Guid } from 'guid-typescript';
  * @prop [rules] Rules for equipping; profession and level requirements, etc.
  *
  */
-export abstract class Equipment extends Item {
-  slot: string;
+export class Equipment extends Item {
+  id: Guid;
+  name: string;
   stats: object;
-  rules: object;
+  slot: EquipSlot;
+  rules: EquipRules;
 
-  constructor(params: any) {
+  constructor(params: EquipmentConfiguration) {
     super(params);
     this.slot = params.slot;
     this.stats = params.stats;
     this.rules = params.rules;
   }
 
-  abstract onEquip(): void;
-  abstract onRemove(): void;
+  onEquip(): void {
+    throw new Error("Method not implemented.");
+  }
+  
+  onRemove(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  create(item: object | object[]): Equipment | Equipment[] {
+    throw new Error("Method not implemented.");
+  }
+
+  destroy(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export interface EquipmentConfiguration {
+  name: string;
+  stats: object;
+  slot: EquipSlot;
+  rules: EquipRules;
 }
 
 export interface EquipRules {
-  level?: number | string;
+  level?: number;
   profession?: Guid[];
+}
+
+export enum EquipSlot {
+  'main-hand',
+  'off-hand',
+  'head',
+  'body',
+  'legs',
+  'hands',
+  'feet',
+  'acc'
 }
